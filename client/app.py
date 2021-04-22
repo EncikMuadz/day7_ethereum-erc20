7ea8098a3de2f10e54307c05388a04d3046cb670
EncikMuadz <muadz@twistcode.com>
2021-04-22 09:55:42 +0100

    modified; frontend to call api

--- client/app.py
+++ client/app.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
+import requests
+
 st.write('Dapp App')
+
+url = "http://dapp-back:5005"
+
+endpoint = '/api/connection/status/'
+
+resp = requests.get(url+endpoint)
+
+resp_json = resp.json()
+
+st.write(resp_json)
