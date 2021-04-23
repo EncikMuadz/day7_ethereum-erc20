b710d6f1a1d2984c10f9e321ccd1ea27fec9f2f1
EncikMuadz <muadz@twistcode.com>
2021-04-23 08:55:05 +0100

    added; features for accounts, block

--- client/app.py
+++ client/app.py
@@ -6,10 +6,52 @@ st.write('Dapp App')
 
 url = "http://dapp-back:5005"
 
-endpoint = '/api/connection/status/'
+def home():
+    ''' landing page untuk project'''
+    st.subheader('Rumah')
 
-resp = requests.get(url+endpoint)
+def conn_status():
+    endpoint = '/api/connection/status/'
+    
+    resp = requests.get(url+endpoint)
+    
+    resp_json = resp.json()
+    
+    st.write(resp_json)
 
-resp_json = resp.json()
+def block_num():
+    endpoint = '/api/eth/block/number'
+    
+    resp = requests.get(url+endpoint)
+    
+    resp_json = resp.json()
+    
+    st.write(resp_json)
 
-st.write(resp_json)
+def accounts_known():
+    endpoint = '/api/eth/accounts'
+    
+    resp = requests.get(url+endpoint)
+    
+    resp_json = resp.json()
+    
+    st.write(resp_json)
+
+def main():
+    st.title('Twistcode - Ethflow')
+    menu = [
+        'Home', 
+        'General',
+        'Accounts'
+    ]
+    choice = st.sidebar.selectbox('Menu', menu)
+    if choice == 'General':
+        conn_status()
+        block_num()
+    elif choice == 'Accounts':
+        accounts_known()
+    else:
+        home()
+
+if __name__ == '__main__':
+    main()
